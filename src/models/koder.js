26c4ef6354bac0c3553e0da8145bbf607e85edfb
rrortegaa <rrortegaa@gmail.com>
2019-03-26 02:47:42 -0600

    add Schema koderSchema

--- src/models/koder.js
+++ src/models/koder.js
@@ -3,7 +3,29 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const koderSchema = new Schema({})
+const koderSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String, 
+    required: true,
+    trim: true,
+    lowercase: true
+  }, 
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
 
 const model = mongoose.model('Koder', koderSchema)
 
