cdcd8b85309746a8f5f58ca2d15e124b5cede1bf
charles <charlesipn@gmail.com>
2019-03-26 03:00:01 -0600

    koder model added

--- src/routes/koders.js
+++ src/routes/koders.js
@@ -9,6 +9,21 @@ router.get('/', async (ctx) => {
   ctx.resolve({
     message: `Root koders router`
   })
+
+})
+
+router.post('/login', async ctx => {
+  const { email, password } = ctx.request.body
+
+  if (!email) throw ctx.throw(400, 'Email is required')
+  if (!password) throw ctx.throw(400, 'Password is required')
+
+  
+  
+  console.warn({ email, password })
+  ctx.resolve({
+    message: 'login'
+  })
 })
 
 
