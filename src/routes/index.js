d886e20431dfac69dcc01bfe6c364bd44c02a677
charles <charlesipn@gmail.com>
2019-03-31 02:39:34 -0600

    root message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,21 @@
+const Router = require('koa-router')
+
 const koders = require('./koders')
 
+const root = new Router({
+  prefix: '/'
+})
+
+root.get('/', ctx => {
+  ctx.body = {
+    success: true,
+    message: 'Kodemia API v1',
+    version: '1.0.0'
+  }
+})
+
+
 module.exports = {
-  koders
+  koders,
+  root
 }
