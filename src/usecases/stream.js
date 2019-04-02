2cd3a24dc9f5d48ed613d6ebc58bc039b3e2bb23
Carlos Silva <carlos@leco.mx>
2019-04-02 11:58:20 -0600

    [ADD] create stream endpoint

--- src/usecases/stream.js
+++ src/usecases/stream.js
@@ -1,9 +1,13 @@
 const createError = require('http-errors')
 
 const Stream = require('../models/stream').model
+const Generation = require('../models/generation').model
 
 const create = async ({ name, generation, title, url, muxData, endDate, isActive, isLive }) => {
-  const newStream = new Stream({ name, generation, title, url, muxData, endDate, isActive, isLive })
+  const generationFound = await Generation.findOne({ type: generation.type, number: generation.number })
+  if (!generationFound) throw createError(409, `Generation [${generation.type}, ${generation.number}] does not exists`)
+
+  const newStream = new Stream({ name, generation: generationFound._id, title, url, muxData, endDate, isActive, isLive })
   const error = newStream.validateSync()
   if (error) throw error
 
