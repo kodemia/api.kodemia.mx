6a466b9e808503970dfb2dfcd32181d55422508f
Carlos Silva <carlos@leco.mx>
2019-06-29 22:08:33 -0500

    add now deploy

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const db = require('./src/lib/db')
 const server = require('./src/server')
 const port = process.env.PORT || 8080
 
-server.listen(port, function () {
-  console.info(`Server running in: http://localhost:${port}`)
-  db.connect()
-    .then(() => console.info('< MONGO CONNECTED >'))
-    .catch(error => console.error('ERROR CONN: ', error))
-})
+db.connect()
+  .then(() => {
+    console.info('- DB connected')
+    server.listen(port, console.info(`Server listening on http://localhost:${port}`))
+  })
+  .catch(error => console.error('DB ERROR CONN: ', error))
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB_USER = process.env.DB_USER || ''
+const DB_USER = process.env.DB_USER || 'kodemiadmin'
 const DB_PASSWORD = process.env.DB_PASSWORD || ''
 const DB_NAME = process.env.DB_NAME || 'kodemia'
 const CONN_STRING = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@charles-mongo-cluster-ekbll.mongodb.net/${DB_NAME}?retryWrites=true`
