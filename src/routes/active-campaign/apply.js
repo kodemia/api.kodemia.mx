6a91dd947888915a368fe2f838b72f5c07af040d
charles <charlesipn@gmail.com>
2021-02-19 17:13:55 -0600

    ADD tags support

--- src/routes/active-campaign/apply.js
+++ src/routes/active-campaign/apply.js
@@ -14,12 +14,18 @@ router.post('/', async ctx => {
     phone,
     course,
     customFields = { source: '', reasonToApply: '', campaignName: '' },
-    tags = {}
+    tags = ['website']
   } = ctx.request.body
 
-  const contact = await ac.contacts.upsert(email, firstName, lastName, phone, customFields, tags)
+  const contact = await ac.contacts.upsert(email, firstName, lastName, phone, customFields)
 
   const dealInList = await ac.lists.subscribeContact(contact.id, course)
+
+  const addTagsPromises = tags.map(([tagName]) => {
+    ac.contacts.addTag(contact.id, tagName)
+  })
+
+  Promise.all(addTagsPromises)
 
   ctx.resolve({
     message: 'Contact created and associated',
