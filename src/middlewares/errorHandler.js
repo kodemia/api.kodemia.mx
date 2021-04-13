a9e05821756a9e8d32dcf6b20ac88d37951e958d
arianaomi <arianaomi.lp@gmail.com>
2021-04-13 13:56:11 -0500

    :fire:Remove requestHandle file

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,12 @@
+const Sentry = require('@sentry/node')
 
 module.exports = async (ctx, next) => {
   try {
+    Sentry.getCurrentHub().configureScope(scope =>
+      scope.addEventProcessor(event =>
+        Sentry.Handlers.parseRequest(event, ctx.request, { user: false })
+      )
+    )
     await next()
   } catch (error) {
     ctx.status = error.status || 400
