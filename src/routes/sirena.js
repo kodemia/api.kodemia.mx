53d32625bc924962c2f6c9167a74d5779655f257
arianaomi <arianaomi.lp@gmail.com>
2021-07-15 12:03:55 -0500

    sirena

--- src/routes/sirena.js
+++ src/routes/sirena.js
@@ -13,17 +13,15 @@ router.get('/', ctx => {
 
 router.post('/send-message', async ctx => {
   console.log(ctx.request.body)
-  // const { email } = ctx.request.body
+  const { email } = ctx.request.body
 
-  // if (!email) throw ctx.throw(400, 'Email is required')
-  // await getProspectId(email)
+  if (!email) throw ctx.throw(400, 'Email is required')
+  await getProspectId(email)
 
   // console.log(email)
   ctx.resolve({
     message: email
   })
 })
-
-router.post
 
 module.exports = router
