194b5c7ff0c0a6500ae3c5cab17eeebe08fa5206
charles <charlesipn@gmail.com>
2021-07-16 02:26:24 -0500

    fix template

--- src/usecases/offer.js
+++ src/usecases/offer.js
@@ -3,6 +3,9 @@ const fs = require('fs-extra')
 const Handlebars = require('../lib/handlebars')
 const docusign = require('../lib/docusign')
 
+const offerDocumentTemplateHTML = fs.readFileSync('./src/templates/offer.hbs', 'utf8')
+const offerDocumentTemplate = Handlebars.compile(offerDocumentTemplateHTML)
+
 const defaultOfferData = {
   signerName: null,
   startBootcampDate: null,
@@ -15,14 +18,6 @@ const defaultOfferData = {
 }
 
 async function send (applicantEmail, applicantName, offerData = defaultOfferData) {
-  let fileUrl = './src/templates/offer.hbs'
-  if (process.env.VERCEL) {
-    fileUrl = `.${fileUrl}`
-  }
-
-  console.log('fileUrl: ', fileUrl, process.cwd())
-  const offerDocumentTemplateHTML = fs.readFileSync(fileUrl, 'utf8')
-  const offerDocumentTemplate = Handlebars.compile(offerDocumentTemplateHTML)
   const offerDocumentString = offerDocumentTemplate(offerData)
   return docusign.sendDocumentToBeSigned(applicantEmail, applicantName, offerDocumentString)
 }
