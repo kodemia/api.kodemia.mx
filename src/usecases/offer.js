7d7d2c3c5bde846ad0b854ce58ff08448b4829ee
charles <charlesipn@gmail.com>
2021-07-16 01:56:15 -0500

    new vercel runner

--- src/usecases/offer.js
+++ src/usecases/offer.js
@@ -3,9 +3,6 @@ const fs = require('fs-extra')
 const Handlebars = require('../lib/handlebars')
 const docusign = require('../lib/docusign')
 
-const offerDocumentTemplateHTML = fs.readFileSync('./src/templates/offer.hbs', 'utf8')
-const offerDocumentTemplate = Handlebars.compile(offerDocumentTemplateHTML)
-
 const defaultOfferData = {
   signerName: null,
   startBootcampDate: null,
@@ -18,6 +15,8 @@ const defaultOfferData = {
 }
 
 async function send (applicantEmail, applicantName, offerData = defaultOfferData) {
+  const offerDocumentTemplateHTML = fs.readFileSync('./src/templates/offer.hbs', 'utf8')
+  const offerDocumentTemplate = Handlebars.compile(offerDocumentTemplateHTML)
   const offerDocumentString = offerDocumentTemplate(offerData)
   return docusign.sendDocumentToBeSigned(applicantEmail, applicantName, offerDocumentString)
 }
